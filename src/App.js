4487e91a1ca8367357c1d6b66ff9411a827cd92b
sh4869 <nobuk4869@gmail.com>
2018-06-03 13:41:45 +0900

    Add react-router-example

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+class TestComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is Test Component.</p>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            <Link to="/test">to test</Link>
+          </div>
+          <Route path="/test" component={TestComponent} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
