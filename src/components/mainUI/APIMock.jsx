f5ab0ba3252edfd922e2fc81e9b5fa75e6e2920e
sh4869 <nobuk4869@gmail.com>
2018-06-17 18:41:08 +0900

    Add APIMock.fusens,getFusen() and updateFusen()

--- src/components/mainUI/APIMock.jsx
+++ src/components/mainUI/APIMock.jsx
@@ -1,52 +1,88 @@
 class APIMock {
   static getAllFusen(userID) {
-    let fusenArray = [];
-    for (let i = 0; i < 10; i++) {
-      const fusenData = {
-        userID: userID,
-        fusenID: i,
-        title: `title${i}`,
-        tag: ['tag1', 'tag2'],
-        text: `テキストテキストテキスト${i}`,
-        color: 'ffffff'
-      };
-      fusenArray.push(fusenData);
+    if (this.fusens.length == 0) {
+      let fusenArray = [];
+      for (let i = 0; i < 10; i++) {
+        const fusenData = {
+          userID: userID,
+          fusenID: i,
+          title: `title${i}`,
+          tag: ["tag1", "tag2"],
+          text: `テキストテキストテキスト${i}`,
+          color: "ffffff"
+        };
+        fusenArray.push(fusenData);
+      }
+      this.fusens = fusenArray;
     }
-
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (Math.random() < 1) resolve(fusenArray); //確率で成功
+        //確率で成功
+        if (Math.random() < 1) resolve(this.fusens);
         else reject();
       }, 100);
+    });
+  }
+
+  static getFusen(fusenID) {
+    let fusen = this.fusens.find(v => v.fusenID == fusenID);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (fusen === undefined) {
+          reject();
+        } else {
+          resolve(fusen);
+        }
+      });
     });
   }
 
   static createFusen(userID) {
     const fusen = {
-      'userID': userID,
-      'fusenID': 100 + Math.floor(Math.random() * 100), //めんどいのでIDはランダム
-      'title': ``,
-      'tag': [],
-      'text': ``,
-      'color': 'ffffff'
+      userID: userID,
+      fusenID: 100 + Math.floor(Math.random() * 100), //めんどいのでIDはランダム
+      title: ``,
+      tag: [],
+      text: ``,
+      color: "ffffff"
     };
-
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (Math.random() < 1) resolve(fusen); //確率で成功
-        else reject();
+        this.fusens.push(fusen);
+        resolve(fusen);
       }, 100);
     });
   }
 
   static deleteFusen(userID, fusenID) {
+    const i = this.fusens.findIndex(v => v.fusenID == fusenID);
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (Math.random() < 1) resolve(); //確率で成功
-        else reject();
+        if (i == -1) {
+          reject();
+        } else {
+          this.fusens.splice(i, 1);
+          resolve();
+        }
+      }, 100);
+    });
+  }
+
+  static updateFusen(fusen) {
+    const i = this.fusens.findIndex(v => v.fusenID == fusen.fusenID);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (i == -1) {
+          reject();
+        } else {
+          this.fusens[i] = fusen;
+        }
       }, 100);
     });
   }
 }
+
+// ちょっと汚いけどMockだから許して
+APIMock.fusens = [];
 
 export default APIMock;
