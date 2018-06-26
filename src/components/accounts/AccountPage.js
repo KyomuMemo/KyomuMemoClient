e474af78fb84062a4e22039944acca11cee73b1b
sh4869 <nobuk4869@gmail.com>
2018-06-26 12:20:04 +0900

    Add onAccountIDUpdate props that calls when success to login or register

--- src/components/accounts/AccountPage.js
+++ src/components/accounts/AccountPage.js
@@ -4,20 +4,47 @@ import RegisterFormCompoent from "./RegisterForm";
 import LoginFormCompoent from "./LoginForm";
 import Typography from "@material-ui/core/Typography";
 
+const style = {
+  root: {
+    background: "rgba(0,0,0,0.5)",
+    bottom: 0,
+    left: 0,
+    overflow: "auto",
+    position: "fixed",
+    right: 0,
+    top: 0,
+    zIndex: 2147483647 - 2,
+    padding: "10%"
+  }
+};
+
 class AccountPage extends Component {
   componentDidMount() {
     // 初回時はLoginページに飛ぶ
     this.props.history.push("/account/login");
   }
+  onAccountIDUpdate = async id => {
+    await this.props.onAccountIDUpdate(id);
+    this.props.history.push("/")
+  }
   render() {
     return (
-      <div>
-        <Typography variant="display2" gutterBottom>
-          Fusen Memo Service
-        </Typography>
+      <div style={style.root}>
         <Switch>
-          <Route path="/account/login" component={LoginFormCompoent} />
-          <Route path="/account/register" component={RegisterFormCompoent} />
+          <Route
+            path="/account/login"
+            render={props => (
+              <LoginFormCompoent onAccountIDUpdate={this.onAccountIDUpdate} />
+            )}
+          />
+          <Route
+            path="/account/register"
+            render={props => (
+              <RegisterFormCompoent
+                onAccountIDUpdate={this.onAccountIDUpdate}
+              />
+            )}
+          />
         </Switch>
       </div>
     );
