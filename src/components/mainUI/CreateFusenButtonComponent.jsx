a0962002eb999737a616e9c632107fdefa565ae8
sienori <iilll7d6f@yahoo.co.jp>
2018-07-01 19:51:52 +0900

    Rewrite create fusen button with SFC

--- src/components/mainUI/CreateFusenButtonComponent.jsx
+++ src/components/mainUI/CreateFusenButtonComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "@material-ui/core/Button";
 import AddIcon from "@material-ui/icons/Add";
 
@@ -11,22 +11,18 @@ const styles = {
   }
 };
 
-class CreateFusenButtonComponent extends Component {
-  render() {
-    return (
-      <div>
-        <Button
-          variant="fab"
-          color="secondary"
-          style={styles.button}
-          aria-label="Create"
-          onClick={this.props.createFusen}
-        >
-          <AddIcon />
-        </Button>
-      </div>
-    );
-  }
-}
+const CreateFusenButtonComponent = props => (
+  <div>
+    <Button
+      variant="fab"
+      color="secondary"
+      style={styles.button}
+      aria-label="Create"
+      onClick={props.createFusen}
+    >
+      <AddIcon />
+    </Button>
+  </div>
+);
 
 export default CreateFusenButtonComponent;
